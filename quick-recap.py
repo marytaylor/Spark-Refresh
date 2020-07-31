60039e002cf1956f895e2818e2ea51634971b49c
Mary Taylor <marytaylor@users.noreply.github.com>
2020-07-31 14:59:03 -0400

    Create quick-recap.py

---
+++ quick-recap.py
@@ -0,0 +1,57 @@
+# ------ View Spark table -------------
+
+# Print the tables in the catalog
+print(spark.catalog.listTables())
+
+# ----- Run a Spark SQL query -------------
+
+# Don't change this query
+query = "FROM flights SELECT * LIMIT 10"
+
+# Get the first 10 rows of flights
+flights10 = spark.sql(query)
+
+# Show the results
+flights10.show()
+
+# ---- Store Spark data in dataframe -------
+
+# Don't change this query
+query = "SELECT origin, dest, COUNT(*) as N FROM flights GROUP BY origin, dest"
+
+# Run the query
+flight_counts = spark.sql(query)
+
+# Convert the results to a pandas DataFrame
+pd_counts = flight_counts.toPandas()
+
+# Print the head of pd_counts
+print(pd_counts.head())
+
+# ------Store data in Spark ----------------
+
+# Create pd_temp
+pd_temp = pd.DataFrame(np.random.random(10))
+
+# Create spark_temp from pd_temp
+spark_temp = spark.createDataFrame(pd_temp)
+
+# Examine the tables in the catalog
+print(spark.catalog.listTables())
+
+# Add spark_temp to the catalog
+spark_temp.createOrReplaceTempView("temp")
+
+# Examine the tables in the catalog again
+print(spark.catalog.listTables())
+
+# ----- directly store data without pandas ------------
+
+# Don't change this file path
+file_path = "/usr/local/share/datasets/airports.csv"
+
+# Read in the airports data
+airports = spark.read.csv(file_path, header=True)
+
+# Show the data
+airports.show()
